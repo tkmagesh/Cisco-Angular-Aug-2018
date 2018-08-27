c2f8fd10186f3a6146b6ce3188e540a2746037ba
Magesh <tkmagesh77@gmail.com>
2018-08-27 15:39:44 +0530

    Calculator one solution

---
+++ first-app/src/app/calculator/calculatorOne.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CalculatorModel } from './CalculatorModel';
+
+
+@Component({
+	selector : 'app-calculator-1',
+	templateUrl : './calculatorOne.component.html',
+	styleUrls : ['./calculatorOne.component.css']
+})
+export class CalculatorOneComponent{
+	
+	model : CalculatorModel = new CalculatorModel();
+}
