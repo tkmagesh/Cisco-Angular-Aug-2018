965fa323902c75b658a0aa404e5434f73f2037e0
Magesh <tkmagesh77@gmail.com>
2018-08-28 10:47:26 +0530

    bugTracker template

--- first-app/src/app/calculator/calculatorResult.component.ts
+++ first-app/src/app/calculator/calculatorResult.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
 	selector : 'app-calculator-result',
 	template : `
-		<div class="result" [ngClass]="{positive : data >=0, negative : data < 0}">{{data}}</div>
+		<div class="result" [ngClass]="{positive : data >=0, negative : data < 0}">{{data | currency:'INR'}}</div>
 	`,
 	styleUrls : ['./calculatorResult.component.css']
 })
