007205b19742fc9b209925185d1e5436a5665061
Magesh <tkmagesh77@gmail.com>
2018-08-28 15:06:54 +0530

    sort implementation

--- bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
+++ bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
@@ -8,6 +8,16 @@ import { Bug } from './models/Bug';
 export class BugTrackerComponent{
 	bugs : Bug[] = [];
 
+	sortBy : string = 'name';
+	sortDesc : boolean = false;
+	
+	constructor(){
+		this.bugs.push({name : 'Server communication failre', isClosed : false});
+		this.bugs.push({name : 'User actions not recognised', isClosed : true});
+		this.bugs.push({name : 'Application not responding', isClosed : false});
+		this.bugs.push({name : 'Data integrity checks failed', isClosed : false});
+	}
+
 	onAddNewClick(newBugName : string){
 		let newBug : Bug = {
 			name : newBugName,
