6c4ec71db622d4f6e8e99df5c4a560485bb86689
Magesh <tkmagesh77@gmail.com>
2018-08-29 13:52:58 +0530

    introducing utils module

--- bug-tracker-app/src/app/app.module.ts
+++ bug-tracker-app/src/app/app.module.ts
@@ -2,22 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { UtilsModule } from './utils/utils.module';
+
 import { AppComponent } from './app.component';
 import { BugTrackerComponent } from './bugTracker/bugTracker.component';
 import { BugStatsComponent } from './bugTracker/views/bugStats.component';
 import { BugEditComponent } from './bugTracker/views/bugEdit.component';
 
 import { ClosedCountPipe } from './bugTracker/pipes/closedCount.pipe';
-import { TrimTextPipe } from './bugTracker/pipes/trimText.pipe';
-import { SortPipe } from './bugTracker/pipes/sort.pipe';
-import { ElapsedPipe } from './bugTracker/pipes/elapsed.pipe';
 
 import { BugOperationsService } from './bugTracker/services/bugOperations.service';
 import { BugStorageService } from './bugTracker/services/bugStorage.service';
-
-import * as moment from 'moment';
-
-console.log(moment('2018-08-29T04:26:44.603Z').fromNow());
 
 @NgModule({
   declarations: [
@@ -26,13 +21,11 @@ console.log(moment('2018-08-29T04:26:44.603Z').fromNow());
     , BugStatsComponent
     , BugEditComponent
     , ClosedCountPipe
-    , TrimTextPipe
-    , SortPipe
-    , ElapsedPipe
   ],
   imports: [
     BrowserModule
     , FormsModule
+    , UtilsModule
   ],
   providers: [
   	BugOperationsService,
